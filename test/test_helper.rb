7f6cd1f756b5f167b1dc442256aeae64d8d52a26
nabeki1224 <watanabe.kyo19991224@gmail.com>
2024-06-04 16:31:39 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,11 +7,14 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new(color: true)
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-    # Add more helper methods to be used by all tests here...
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
+
+  
 end
